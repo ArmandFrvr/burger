c50a052f7a71ef95075ca42863b680761c1b8a35
Alwyn Durham <alwyn.durham@evisions.com>
2018-01-23 17:19:08 -0800

    set up server, config, database files

---
+++ app/config/orm.js
@@ -0,0 +1,39 @@
+var connection = require("./connection.js");
+
+var orm = {
+
+  selectAll: function(table, cb) {
+
+    let query = "SELECT * from ??";
+    let params = [table];
+
+    connection.query(query, params, function(err, result) {
+      if(err) throw err;
+      cb(result);
+    });
+  },
+
+  insertOne: function(table, field, value, cb) {
+
+    let query = "INSERT INTO ?? SET ? = ?";
+    let params = [table, field, value];
+
+    connection.query(query, params, function(err, result) {
+      if(err) throw err;
+      cb(result);
+    });
+  },
+
+  updateOne: function(table, field, newValue, whereField, whereValue, cb) {
+
+    let query = "UPDATE ?? SET ? = ? where ? = ?";
+    let params = [table, field, newValue, whereField, whereValue];
+
+    connection.query(query, params, function(err, result) {
+      if(err) throw err;
+      cb(result);
+    });
+  }
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, eaten)
+VALUES ("Thai Beansprout Patty", false),
+("California Veggie Patty", false),
+("Western Bacon Bean Bun", false),
+("Oniony Onionburger with Sesame", true),
+("Shredded Carrot and Corn Patty", true);
